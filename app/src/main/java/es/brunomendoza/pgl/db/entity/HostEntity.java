fe2148576a6250f1363bc33406d64de45c4ee9c0
Bruno Mendoza <hola@brunomendoza.es>
2018-06-02 13:03:50 +0200

    Testing 'develop' branch

--- app/src/main/java/es/brunomendoza/pgl/db/entity/HostEntity.java
+++ app/src/main/java/es/brunomendoza/pgl/db/entity/HostEntity.java
@@ -1,6 +1,30 @@
 package es.brunomendoza.pgl.db.entity;
 
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "host")
 public class HostEntity {
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
     private Long id;
     private String name;
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+//    public void setId(@NonNull Long id) {
+//        this.id = id;
+//    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
