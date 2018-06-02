b382f80feca1574a9bed9b06852dbaa187224429
Bruno Mendoza <hola@brunomendoza.es>
2018-06-02 20:50:36 +0200

    Iface entity added

--- app/src/main/java/es/brunomendoza/pgl/db/entity/IfaceEntity.java
+++ app/src/main/java/es/brunomendoza/pgl/db/entity/IfaceEntity.java
@@ -1,6 +1,62 @@
 package es.brunomendoza.pgl.db.entity;
 
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+import android.support.annotation.NonNull;
+
+@Entity(tableName = "iface")
 public class IfaceEntity {
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "iface_id")
     private Long id;
+
     private String name;
+
+    private String ipAddress;
+
+    @NonNull
+    private String mac;
+
+    @NonNull
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(@NonNull Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
+    @NonNull
+    public String getMac() {
+        return mac;
+    }
+
+    public void setMac(@NonNull String mac) {
+        this.mac = mac;
+    }
+
+    public IfaceEntity(@NonNull Long id, String name, String ipAddress, @NonNull String mac) {
+        this.id = id;
+        this.name = name;
+        this.ipAddress = ipAddress;
+        this.mac = mac;
+    }
 }
